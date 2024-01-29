c900d55c83a9ef26f823026784301fee50c8fdb2
Shubham Karmveer <s.karmveer1201@gmail.com>
2024-01-30 01:13:04 +0530

    Create BinarySearch.cpp

---
+++ Searching_Sorting/BinarySearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+  int start = 0;
+  int end = n-1;
+  
+ int  mid=start+(end-start)/2;
+  while(start <= end) {
+    if(arr[mid] == target) {
+      return mid;
+    }
+    else if(target > arr[mid]) {
+      start = mid + 1;
+    }
+    else if(target < arr[mid]) {
+      end = mid - 1;
+    }
+    mid =start+(end-start)/2;
+  }
+  return -1;
+}
+
+int main(){
+  
+  int arr[] = {1,2,3,4,5,6,7,8};
+  int target = 6;
+  int n=8;
+  int index=0;
+  
+  if(index=binarySearch(arr,n,target)){
+    cout<<"Element found at index: "<<index;
+  }else{
+    cout<<"Element not found.";
+  }
+  return 0;
+
+}
